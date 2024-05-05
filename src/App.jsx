e00656df5f9c7161debbc03bdfadd5aa4f9e6fc4
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-05 14:45:15 +0100

    updated the Card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import {} from 'react'
 import './App.css'
 import Navbar from './Navbar'
 import Header from './Header'
+import Card from './Card'
 
 const App = () => {
   return (
     <>
     <Header/>
     <Navbar/>
+    <Card/>
     </>
   )
 }
