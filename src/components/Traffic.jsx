4854abd06846757d41c6aad8df3ec303910ea97b
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-27 02:18:15 +0100

    updates lights

--- src/components/Traffic.jsx
+++ src/components/Traffic.jsx
@@ -5,11 +5,29 @@ import { useState } from 'react'
 // Create a component for a traffic light that can be in three states: "red", "yellow", and "green". Use state to manage the current light color and buttons to change between them.
 
 const Traffic = () => {
-const [states, setStates] = useState()
+    const [color, setColor] = useState('red'); // Current color (string)
+
+
+    function handleClicks(){
+        switch(color){
+        case 'red':
+        setColor('yellow');
+        break;
+        case 'yellow':
+        setColor('green');
+        break;
+        default:
+        setColor('red'); // Reset to red from green
+        }
+    }
+
 
   return (
     <div>
-      
+      <div>
+    <h2>Color: {color}</h2>
+    <button onClick={handleClicks}>Click</button>
+  </div>
     </div>
   )
 }
