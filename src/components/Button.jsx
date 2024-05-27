c571445c7dcdfbdfeef9d6ce3a803b40b580590f
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-27 01:01:15 +0100

    updates toggle

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Button = () => {
+
+ const [time, setTime] = useState(true)
+
+
+
   return (
-    <div>
+    <div className='shift'>
       <button className='light'>Toggle</button>
       <h2>The light is: </h2>
     </div>
