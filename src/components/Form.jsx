650060dfb07df3475123fb1b5080ef8ad7e3be3e
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-29 14:02:22 +0100

    update form change

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = ({setInputText}) => {
+const Form = () => {
 
-const inputTextHandler = (e) =>{
-  setInputText(e.target.value)
-}
+  const [text, setText] = useState("");
+
+  const textChange = (e) =>{
+    setText(e.target.value)
+  }
 
 
   return (
     <div>
       <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <input onChange={textChange} type="text" className="todo-input" />
+
       <button className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
+
       <div className="select">
+
         <select name="todos" className="filter-todo">
+
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
+
         </select>
       </div>
     </form>
