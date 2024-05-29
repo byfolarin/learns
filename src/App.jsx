f172b193dc989846f98ab07da38415020e670ff6
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-29 11:33:06 +0100

    todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
-import Thermostat from './components/Thermostat';
-import Traffic from './components/Traffic';
 import ToDoItem from './components/TodoItem';
 
 
 function App() {
 
-  const [inputText, setInputText] = useState('');
+
  
   return (
       <div className="App">
         <header>
           <h1>Todo List</h1>
         </header>
-        <Form setInputText ={setInputText} inputText={inputText}/>
+        <Form/>
         <TodoList/>
-        <Thermostat/>
-        <Traffic/>
         <ToDoItem/>
       </div>
   );
