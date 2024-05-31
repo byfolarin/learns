46ab2ac41ded4ba3a11c39252ba9903f07416b36
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-01 00:23:25 +0100

    making TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import TodoItem from './components/TodoItem';
 
 
 
@@ -7,7 +8,7 @@ function App() {
  
   return (
       <div className="App">
-  
+        <TodoItem/>
       </div>
   );
 }
