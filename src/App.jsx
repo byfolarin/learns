4b5c10731d63fbf213336b5e11451c5c35640ea5
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-01 00:14:16 +0100

    updating files of todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
 import './App.css';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
-import ToDoItem from './components/TodoItem';
-import { useState } from 'react';
+
 
 
 function App() {
 
-  const [list, setList] = useState([]);
-  const [input, setInput] = useState("");
-  const [status, setStatus] = useState("all")
-  const [filteredTodos, setFilteredTodos] = useState([])
-
-
-  
-  function filterHandler(){
-    switch(status){
-      case 'completed':
-        setFilteredTodos(list.filter((todo) => todo.completed === true))
-        break;
-
-      case 'uncompleted':
-        setFilteredTodos(list.filter((todo) => todo.completed === false))
-        break;
-
-      default:
-        setFilteredTodos(list)
-        break;
-    }
-  }
-
  
   return (
       <div className="App">
-        <header>
-          <h1>Todo List</h1>
-        </header>
-        <Form 
-            input={input}
-            setInput={setInput}
-            list={list}
-            setList={setList}
-            setStatus={setStatus}/>
-        <TodoList/>
-        <ToDoItem/>
+  
       </div>
   );
 }
