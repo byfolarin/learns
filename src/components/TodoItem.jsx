bd5e3901dd118178ab5c1a0297b4c4c4403b85df
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-01 00:49:28 +0100

    updating components

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const TodoItem = () => {
   return (
-    <div>
-      <input type="text" className='form-input' name="" id="" />
-      <div className='add-to-list'>
-                <i className='fas fa-plus'>        </i>
-            </div>
+    <div className='sets-on'>
+        <div className="container">
+              <input type="text" className='form-input' name="" id="" />
+                <div className='add-to-list'>
+                <i className='fas fa-plus'></i>
+        </div>
+    </div>
     </div>
   )
 }
