c638cd847b3dadd7dec4dbef29c7ae4cce8f603d
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-01 01:01:04 +0100

    select values

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,11 +5,22 @@ const TodoItem = () => {
     <div className='sets-on'>
         <div className="container">
               <input type="text" className='form-input' name="" id="" />
-                <div className='add-to-list'>
-                <i className='fas fa-plus'></i>
+                <div className='add-to-list'> <i className='fas fa-plus'></i>
         </div>
+        </div>
+
+
+        <div className='select'>
+                <select>
+                    <option value='all'>All</option>
+                    <option value='completed'>Completed</option>
+                    <option value='uncompleted'>Uncompleted</option>
+                </select>
+            </div>
+
     </div>
-    </div>
+
+    
   )
 }
 
