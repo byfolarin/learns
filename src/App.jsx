d94f0d943e95381221dcf0f2f4b0eaf96c887eab
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-02 09:01:21 +0100

    let's go

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import FormInput from './components/FormInput';
+import Todo from './components/Todo';
 
 
 function App() {
@@ -8,6 +9,24 @@ function App() {
   const [input,setInput] = useState("")
   const [status,setStatus] = useState("all")
   const [list,setList] = useState([])
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+
+  function filterHandler(){
+    switch(status){
+      case 'completed':
+        setFilteredTodos(list.filter((todo) => todo.completed === true))
+        break;
+
+      case 'uncompleted':
+        setFilteredTodos(list.filter((todo) => todo.completed === false))
+        break;
+
+      default:
+        setFilteredTodos(list)
+        break;
+    }
+  }
 
 
 
@@ -27,7 +46,8 @@ function App() {
             setList={setList}
             />
 
-            <TodoList />
+
+    <Todo input = {input} />
 
       </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react'
 
-const Todo = () => {
+const Todo = ({input}) => {
 
 
   return (
     <div>
-      
+      <div className='task-body'>
+            <li className='task-to-do'>
+                {input}
+            </li>
+            <div className='check'>
+                <i className='fas fa-check'></i>
+            </div>
+            <div className='trash'>
+                <i className='fas fa-trash'></i>
+            </div>
+       </div>
     </div>
   )
 }
