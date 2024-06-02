96be7afcd9d0a6b33ffe62c7758aaec76b25a37f
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-02 06:45:03 +0100

    todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,11 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function Todo() {
+const Todo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-//   return (
-//     <div>
-//     </div>
-//   )
-// }
+export default Todo
