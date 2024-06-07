0d1cda3d8440876d6a1b3a902cd377a26a41a751
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-08 00:09:12 +0100

    init image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
 import { useState } from 'react';
 import './App.css';
-import FormInput from './components/FormInput';
-import TodoList from './components/TodoList';
-import { Routes, Route } from 'react-router-dom';
-
-
 
 function App() {
 
-  const [input,setInput] = useState("")
-  const [status,setStatus] = useState("all")
-  const [list,setList] = useState([])
-  const [filteredTodos, setFilteredTodos] = useState([])
-
-
-  function filterHandler(){
-    switch(status){
-      case 'completed':
-        setFilteredTodos(list.filter((todo) => todo.completed === true))
-        break;
-
-      case 'uncompleted':
-        setFilteredTodos(list.filter((todo) => todo.completed === false))
-        break;
-
-      default:
-        setFilteredTodos(list)
-        break;
-    }
-  }
 
 
 
@@ -38,23 +12,7 @@ function App() {
     
       <div className="App">
 
-            <Routes>
-            <header>
-              <h1>Fola's To-Do list</h1>
-            </header>
-
-            <FormInput 
-            input = {input}
-            setInput={setInput}
-            status={status}
-            setStatus={setStatus}
-            list = {list}
-            setList={setList}
-            />
-            </Routes>
-
-
-    <TodoList />
+           <h1>Images</h1>
 
       </div>
   );
