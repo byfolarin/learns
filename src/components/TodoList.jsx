274d56e56a07a893366a46b284edc9b1fa4623b4
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-14 03:54:23 +0100

    handleChange

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({input, setInput}) => {
+
+
+    function handleChange (e){
+        setInput(e.target.value)
+    }
+
+
+
   return (
     <div>
         <label htmlFor="list-text">Input your list</label>
-      <input type="text" id='list-text' placeholder='input your list item'/>
+      <input type="text" value={input} id='list-text' placeholder='input your list item'/>
       <button>Add item</button>
     </div>
   )
-}
+ }
 
 export default TodoList
