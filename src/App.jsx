c0f41c9dc76a77c4c0547e274363757ef61d515f
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-14 03:22:15 +0100

    list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 
 function App() {
 
+  const [input, setInput] = useState ("")
+
   return (
     
       <div className="App">
-        
+        <TodoList input={input} setInput={setInput}/>
       </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const TodoList = () => {
   return (
     <div>
-      
+        <label htmlFor="list-text">Input your list</label>
+      <input type="text" id='list-text' placeholder='input your list item'/>
+      <button>Add item</button>
     </div>
   )
 }
