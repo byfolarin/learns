7fb4d143e0a94e0ba89a9eb8e3c692d1758f57d6
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-18 16:34:18 +0100

    editing layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 
@@ -6,12 +6,24 @@ import TodoList from './components/TodoList';
 
 function App() {
 
+  const [input, setInput] = useState ("");
+  const [list, setList] = useState([]);
+  const [arr, setArr] = useState([])
 
 
   return (
     
       <div className="App">
-        <TodoList/>   
+        <TodoList input={input}
+                    setInput={setInput} 
+                    list= {list}
+                    setList={setList}
+                    arr = {arr}
+                    setArr ={setArr}
+        />
+
+
+        <TodoItem/>
       </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,32 +1,13 @@
 import React from 'react'
-import { useState } from 'react';
+
 
 const TodoList = () => {
 
-    const [input, setInput] = useState ("");
-    const [list, setList] = useState([]);
-    const [arr, setArr] = useState([])
-
-
-    function handleChange (e){
-        setInput(e.target.value)
-    }
-
-    function handleList (){
-        setList(prev =>[...prev, input])
-        setArr([...list])
-        setInput('')
-    }
-
+   
 
   return (
     <div>
-        <label htmlFor="list-text">Input your list</label>
-      <input type="text" value={input} onChange={handleChange} placeholder='input your list item'/>
-      <button onClick={handleList}>Add item</button>
-
-      <p>{arr.map(i =>(<li>{i}</li>))}</p>
-
+        
     </div>
 
     
