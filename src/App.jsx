1fc023da9042a613c4ac84bec3697247345b983e
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-02 11:06:44 +0100

    enumeration list items

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,21 @@ import './App.css';
 
 function App() {
 
+
+
   return (
     <>
+    <ul>
+      <li>Home</li>
+      <li>Search</li>
+      <li>Explore</li>
+      <li>Reels</li>
+      <li>Messages</li>
+      <li>Notifications</li>
+      <li>Create</li>
+      <li>Profile</li>
+      <li>More</li>
+    </ul>
     </>
       
   )
