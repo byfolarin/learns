53bfc67d6232f35137e518f45e63caeb5c958b9e
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-03 09:19:52 +0100

    layout add

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,16 @@ function App() {
     <>
     <BrowserRouter>
     
-    <ul>
-    <li><Link to={"/"}>Home</Link></li>
-    <li><Link to={"/"}>Search</Link></li>
-    <li><Link to={"/"}>Explore</Link></li>
-    <li><Link to={"/"}>Reels</Link></li>
-    <li><Link to={"/"}>Messages</Link></li>
-    <li><Link to={"/"}>Notification</Link></li>
-    <li><Link to={"/"}>Create</Link></li>
-    <li><Link to={"/"}>Profile</Link></li>
-    <li><Link to={"/"}>More</Link></li>
+    <ul className='style-side-bar'>
+    <li className='side--bar-list-items'><Link to={"/"}>Home</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>Search</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>Explore</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>Reels</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>Messages</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>Notification</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>Create</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>Profile</Link></li>
+    <li className='side--bar-list-items'><Link to={"/"}>More</Link></li>
     </ul>
 
     <>
