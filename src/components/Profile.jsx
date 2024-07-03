184f9d4a95561bb54eeb0442ac7d74099dc79af6
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-03 11:48:08 +0100

    profile marker

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,55 +11,62 @@ const Profile = () => {
 
       <div className="profile-header-section">
 
-
-          <div className="img-section-edit">
-              <div className="img-container-profile">
-              </div>
-          </div>
+                <div className="profile-marker">
 
 
-
-      <div className="wrap-profile-edit">
-
-              <div className="name-change">
-                <h2>byfolarin</h2>
-              </div>
+                  <div className="img-section-edit">
+                      <div className="img-container-profile">
+                      </div>
+                  </div>
 
 
-              <div className="lump-edit">
-              <div className="profile-edit btn-profile">
-              Edit Profile
-              </div>
+                      <div className="top-nav-wrapper">
 
-              <div className="archive-view btn-profile">
-              View Archive
-              </div>
+                      <div className="wrap-profile-edit">
 
-              <div className="ad-tools btn-profile">
-              Ad Tools
-              </div>
-              </div>
-
-      </div>
+                              <div className="name-change">
+                                <h2>byfolarin</h2>
+                              </div>
 
 
-      
-      <div className="profile-count-edit">
+                              <div className="lump-edit">
+                              <div className="profile-edit btn-profile">
+                              Edit Profile
+                              </div>
 
-              <div className="post-change">
-                <h2>8 post</h2>
-              </div>
+                              <div className="archive-view btn-profile">
+                              View Archive
+                              </div>
+
+                              <div className="ad-tools btn-profile">
+                              Ad Tools
+                              </div>
+                              </div>
+
+                      </div>
+
+
               
-              <div className="profile-followers-change">
-              194 followers
-              </div>
+        </div>
 
-              <div className="profile-following-change">
-              449 following
-              </div>
-            
-      </div>
+                              <div className="profile-count-edit">
 
+                                      <div className="post-change">
+                                        8 post
+                                      </div>
+                                      
+                                      <div className="profile-followers-change">
+                                      194 followers
+                                      </div>
+
+                                      <div className="profile-following-change">
+                                      449 following
+                                      </div>
+                                    
+                              </div>
+                              </div>
+      
+           
 
       </div>
 
