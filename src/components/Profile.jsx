a1643ee3186a7006cac8d0ec259a3c6d47aec93c
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-03 10:34:10 +0100

    profile section add

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,26 +10,31 @@ const Profile = () => {
       <div className="profile-container">
 
       <div className="profile-header-section">
-      <div className="img-container-profile">
-      </div>
+
+          <div className="img-section-edit">
+              <div className="img-container-profile">
+              </div>
+          </div>
+
+
 
       <div className="wrap-profile-edit">
 
-      <div className="name-change">
-        <h2>byfolarin</h2>
-      </div>
+              <div className="name-change">
+                <h2>byfolarin</h2>
+              </div>
 
-      <div className="profile-edit btn-profile">
-       Edit Profile
-      </div>
+              <div className="profile-edit btn-profile">
+              Edit Profile
+              </div>
 
-      <div className="archive-view btn-profile">
-       View Archive
-      </div>
+              <div className="archive-view btn-profile">
+              View Archive
+              </div>
 
-      <div className="ad-tools btn-profile">
-      Ad Tools
-      </div>
+              <div className="ad-tools btn-profile">
+              Ad Tools
+              </div>
 
       </div>
 
