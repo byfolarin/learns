aabebfe9442e02e3768d7f018fc248bcbc18fbc6
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-03 09:13:49 +0100

    link tags added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 
@@ -9,17 +9,26 @@ function App() {
 
   return (
     <>
-    <ul className='sidebar-nav'>
-      <li>Home</li>
-      <li>Search</li>
-      <li>Explore</li>
-      <li>Reels</li>
-      <li>Messages</li>
-      <li>Notifications</li>
-      <li>Create</li>
-      <li>Profile</li>
-      <li>More</li>
+    <BrowserRouter>
+    
+    <ul>
+    <li><Link to={"/"}>Home</Link></li>
+    <li><Link to={"/"}>Search</Link></li>
+    <li><Link to={"/"}>Explore</Link></li>
+    <li><Link to={"/"}>Reels</Link></li>
+    <li><Link to={"/"}>Messages</Link></li>
+    <li><Link to={"/"}>Notification</Link></li>
+    <li><Link to={"/"}>Create</Link></li>
+    <li><Link to={"/"}>Profile</Link></li>
+    <li><Link to={"/"}>More</Link></li>
     </ul>
+
+    <>
+    
+    </>
+
+
+  </BrowserRouter>
     </>
       
   )
