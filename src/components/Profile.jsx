521fd57cc2e399936c3bde05262abb40e02d4862
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-03 18:23:36 +0100

    stories archive

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -86,27 +86,27 @@ const Profile = () => {
                               </div>
 
 
+                              
+
+
                               </div>
 
-
-                             
-
+                              
+                              
 
 
        </div>
-                                        <div className="stories-img">
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        <div className="img-stories"></div>
-                                        </div>
   
-      
+       <div className="stories-img">
+                                        <div className="img-stories"></div>
+                                        <div className="img-stories"></div>
+                                        <div className="img-stories"></div>
+                                        <div className="img-stories"></div>
+                                        <div className="img-stories"></div>
+                                        <div className="img-stories"></div>
+                                        <div className="img-stories"></div>
+                            
+                                        </div>
           
 
 
