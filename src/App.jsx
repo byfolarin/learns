205d3eed3bdf0074678d8c2158d3245b97d7327c
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-04 07:39:12 +0100

    postion-side-bar

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,18 @@ function App() {
     <BrowserRouter>
 
     <div className="container">
-    <ul className='style-side-bar'>
-
-    <h2 className='logo-repo'>Instagram</h2>
-    <li className='side--bar-list-items'><Link to={"/"}>Home</Link></li>
-    <li className='side--bar-list-items'><Link to={"/"}>Search</Link></li>
-    <li className='side--bar-list-items'><Link to={"/"}>Explore</Link></li>
-    <li className='side--bar-list-items'><Link to={"/"}>Reels</Link></li>
-    <li className='side--bar-list-items'><Link to={"/"}>Messages</Link></li>
-    <li className='side--bar-list-items'><Link to={"/"}>Notification</Link></li>
-    <li className='side--bar-list-items'><Link to={"/"}>Create</Link></li>
-    <li className='side--bar-list-items'><Link to={"/Profile"}>Profile</Link></li>
-    <li className='side--bar-list-items'><Link to={"/"}>More</Link></li>
-    </ul>
+          <ul className='style-side-bar'>
+                  <h2 className='logo-repo'>Instagram</h2>
+                  <li className='side--bar-list-items'><Link to={"/"}>Home</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/"}>Search</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/"}>Explore</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/"}>Reels</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/"}>Messages</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/"}>Notification</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/"}>Create</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/Profile"}>Profile</Link></li>
+                  <li className='side--bar-list-items'><Link to={"/"}>More</Link></li>
+          </ul>
     
 
    
