499b0570599e2a2abc068877f2b506439615ace6
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-05 15:06:36 +0100

    grid column

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img1 from '../assets/images/insta1.png'
 import img2 from '../assets/images/Dashboard.png'
+import img3 from '../assets/images/cards.png'
 
 const Posts = () => {
   return (
@@ -17,30 +18,10 @@ const Posts = () => {
           </div>
 
           <div className="first-img">
-          <div className="img-container"><img src={img2} alt="" /></div>
+          <div className="img-container"><img src={img3} alt="" /></div>
           </div>
           
         </div>
-
-        <div className="profile-gallery">
-
-    
-                <div className="first-img">
-                <div className="img-container"><img src={img1} alt="" /></div>
-                </div>
-
-                <div className="first-img">
-                <div className="img-container"><img src={img2} alt="" /></div>
-                </div>
-
-                <div className="first-img">
-                <div className="img-container"><img src={img2} alt="" /></div>
-                </div>
-
-                </div>
-
-        
-
      
 
     </div>
