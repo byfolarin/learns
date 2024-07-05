e82b782d81b4edb39b0822eef9fd42bbe04ed513
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-05 14:28:45 +0100

    icons spec

--- src/assets/SavedIcons.jsx
+++ src/assets/SavedIcons.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SavedIcons = () => {
   return (
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" aria-label="" width="24px" height="24px"><title></title><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" aria-label="" width="24px" height="24px" viewBox="0 0 32 32"><title></title><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
     </div>
   )
 }
