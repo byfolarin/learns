bc5863897a0b4f8d6a7c66e00395ed8e60e49457
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-08 11:07:58 +0100

    modal sort

--- src/components/Modals/ProfileModal.jsx
+++ src/components/Modals/ProfileModal.jsx
@@ -4,7 +4,10 @@ const ProfileModal = () => {
   return (
     <div className='modal'>
       <div className="delete-modal">
-        <h2 className='delete-modal-h2'>Change Profile Photo</h2>
+        <h2 className='delete-Change'>Change Profile Photo</h2>
+        <h3 className='delete-Upload post-photo'>Upload Photo</h3>
+        <h3 className='delete-Remove post-photo'>Remove Current Photo</h3>
+        <h3 className='delete-Cancel post-photo'>Cancel</h3>
       </div>
     </div>
   )
