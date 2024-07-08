6e342eeea88e307d287164ba084136246a76753c
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-08 10:31:14 +0100

    img-profile-edit

--- src/components/Profile/ProfileImg.jsx
+++ src/components/Profile/ProfileImg.jsx
@@ -9,11 +9,11 @@ const ProfileImg = ({defaultImageSrc}) => {
 
                                   <div className="img-fit-container">
 
-                                    {/* <div className="img-container-profile" >
-                                    <img id="uploaded-image" src={defaultImageSrc} alt="Profile Image" />
-                                    <input type="file" id="image-upload" accept="image/*" style="display: none;">
+                                    <div className="img-container-profile" >
+                                    <img id="uploaded-image" src={defaultImageSrc} alt="Profile Image" className='img-img-profile' />
+                                    <input type="file" id='image-upload'  accept="image/*"/>
                                     <label for="image-upload">Upload Image</label>
-                                    </div> */}
+                                    </div>
 
                                 </div>
 
