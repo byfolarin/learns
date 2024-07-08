74a6bd26eaafba958da9321cce1c0454c0e34373
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-08 11:01:52 +0100

    modal layout

--- src/components/Modals/ProfileModal.jsx
+++ src/components/Modals/ProfileModal.jsx
@@ -4,7 +4,7 @@ const ProfileModal = () => {
   return (
     <div className='modal'>
       <div className="delete-modal">
-        
+        <h2 className='delete-modal-h2'>Change Profile Photo</h2>
       </div>
     </div>
   )
