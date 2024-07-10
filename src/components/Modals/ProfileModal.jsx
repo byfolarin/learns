f20d3a9ec9649cac562b2e54ecd2202fb59e9f82
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-10 21:26:34 +0100

    states update remove

--- src/components/Modals/ProfileModal.jsx
+++ src/components/Modals/ProfileModal.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const ProfileModal = ({onClose,imgSet,removeSet}) => {
 
-  const fileChange = (e) =>{
-   imgSet(URL.createObjectURL(e.target.files[0]))
-   onClose()
-  }
 
-  const fileRemove = () =>{
-    imgSet(!removeSet)
-  }
+      const [isImageChanged, setIsImageChanged] = useState(false);
 
+        const fileChange = (e) =>{
+        imgSet(URL.createObjectURL(e.target.files[0]))
+        onClose()
+        setIsImageChanged(true)
+        }
+
+        const handleImageReset = () => {
+          setImageUrl(defaultImageUrl);
+          setIsImageChanged(false);
+        };
 
   return (
     <div className='modal' >
@@ -18,12 +22,12 @@ const ProfileModal = ({onClose,imgSet,removeSet}) => {
         <h2 className='delete-Change'>Change Profile Photo</h2>
 
 
-        <label htmlFor='upload-profile'  className='delete-Upload post-photo'>Upload Photo
+        <label htmlFor='upload-profile'  className='delete-Upload post-photo'>
           <input onChange={fileChange} type="file" id='upload-profile' />
         </label>
 
         
-        <h3 className='delete-Remove post-photo' onClick={fileRemove}>Remove Current Photo</h3>
+        <h3 className='delete-Remove post-photo' onClick={handleImageReset}>Remove Current Photo</h3>
         <h3 className='delete-Cancel post-photo' onClick={onClose}> Cancel</h3>
       </div>
     </div>
