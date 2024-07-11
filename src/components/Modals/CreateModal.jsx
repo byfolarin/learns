7c4990cf63c4387d11720e4e18453cdfc2a532eb
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-12 00:22:11 +0100

    create modal click

---
+++ src/components/Modals/CreateModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreateModal = () => {
+  return (
+    <div>
+      <div className="create-new-post">Posts</div>
+      <div className="create-new-live">Live</div>
+    </div>
+  )
+}
+
+export default CreateModal
