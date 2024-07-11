d9ad4fbbcd1fd6c67ef021a0ff8dec363dc5a94f
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-12 00:04:12 +0100

    init state post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img1 from '../assets/images/insta1.png'
 import img2 from '../assets/images/Dashboard.png'
 import img3 from '../assets/images/cards.png'
 
 const Posts = () => {
+
+
+  const [postHold, setPostHold] =useState([])
+
+
+
+
+
   return (
     <div>
          <div className="profile-gallery">
