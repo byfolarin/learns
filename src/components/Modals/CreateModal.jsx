9ff6738b58b10fcea77bc291d2098bc106286c4a
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-17 13:22:53 +0100

    value

--- src/components/Modals/CreateModal.jsx
+++ src/components/Modals/CreateModal.jsx
@@ -5,14 +5,14 @@ import Posts from '../Posts'
 
 const CreateModal = () => {
 
-const [localImg, setLocalImg] = useState([])
+// const [localImg, setLocalImg] = useState([])
 
-const [isImageChanged, setIsImageChanged] = useState(false);
+// const [isImageChanged, setIsImageChanged] = useState(false);
 
-const fileChange = (e) =>{
-setLocalImg(URL.createObjectURL(e.target.files[0]))
-setIsImageChanged(true);
-}
+// const fileChange = (e) =>{
+// setLocalImg(URL.createObjectURL(e.target.files[0]))
+// setIsImageChanged(true);
+// }
 
   return (
     <div className='modal-create'>
